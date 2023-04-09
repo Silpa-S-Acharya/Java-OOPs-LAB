f6ee20e191fff7067708fee3c89ccf952239f819
Acharya Silpa <117834398+Silpa-S-Acharya@users.noreply.github.com>
2023-04-09 11:23:08 +0530

    Update Leapyr.java

--- Leapyr.java
+++ Leapyr.java
@@ -1,16 +1,19 @@
 import java.util.Scanner;
-class Leapyr{
-public static void main(String args[]){
-Scanner sc=new Scanner(System.in);
-int start,end;
-System.out.println("Enter the begining year: ");
-start = sc.nextInt();
-System.out.println("Enter the end year: ");
-end = sc.nextInt();
-System.out.println("Leap years: ");
-for(int i=start;i<=end;i++){
-if(i%4==0||(i%100!=0)&&(i%400==0))
-System.out.println(i);
-}
-}
+class Leapyr
+{
+  public static void main(String args[])
+  {
+    Scanner sc=new Scanner(System.in);
+    int start,end;
+    System.out.println("Enter the begining year: ");
+    start = sc.nextInt();
+    System.out.println("Enter the end year: ");
+    end = sc.nextInt();
+    System.out.println("Leap years: ");
+    for(int i=start;i<=end;i++)
+    {
+        if(i%4==0||(i%100!=0)&&(i%400==0))
+        System.out.println(i);
+    }
+  }
 }
