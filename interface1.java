5a7b2ad1ba806533c356fff05cf908dc2fb889f8
Acharya Silpa <117834398+Silpa-S-Acharya@users.noreply.github.com>
2023-06-12 15:09:15 +0530

    Update interface1.java

--- interface1.java
+++ interface1.java
@@ -1,45 +1,56 @@
 import java.util.*;
-interface Student{
-void set_value1(String sname,int s,String regno);
-} interface Sports{
-void set_value2(float p);
-} class Result implements Student, Sports{
-public String name,reg_no;
-public int total;
-public float score_point;
-public void set_value1(String sname,int s,String regno){
-name=sname;
-total=s;
-reg_no=regno;
-}
-public void set_value2(float pt){
-score_point=pt;
-}
-public void display(){
-System.out.println("Register number: "+ reg_no);
-System.out.println("Name: " + name);
-System.out.println("Academic score: " + total);
-System.out.println("Score obtained in sports: " + score_point);
-}
-37
-} class Interfaceprgrm{
-public static void main(String args[]){
-Scanner sc=new Scanner(System.in);
-Scanner s=new Scanner(System.in);
-int total;float pt;
-System.out.print("Enter the register number: ");
-String reg_no=sc.nextLine();
-System.out.print("Name of the student: ");
-String name=sc.nextLine();
-System.out.println("Enter the academic and sports details of a student: ");
-System.out.print("Enter the total marks obtained(max marks 1200): ");
-total=s.nextInt();
-System.out.println("Enter the overall score point obtained in sports(max
-point 10): ");
-pt=s.nextFloat();
-Result obj=new Result();
-obj.set_value1(name,total,reg_no);
-obj.set_value2(pt);
-obj.display();
-}
+interface Student
+{
+	void read1(String sname,float s,String admsn);
+} 
+interface Sports
+{
+	void read2(float p);
+} 
+class Result implements Student, Sports
+{
+	public String name,admsn;
+	public float total;
+	public float point;
+	public void read1(String sname,float s,String regno)
+	{
+		name=sname;
+		total=s;
+		admsn=regno;
+	}
+	public void read2(float pt)
+	{
+	 	point=pt;
+	}
+	public void display()
+	{
+		System.out.println("Register number: "+admsn);
+		System.out.println("Name: " +name);
+		System.out.println("Academic CGPA: "+total);
+		System.out.println("Score obtained in sports: "+point);
+	}
+
+} 
+class interfaceSS
+{
+	public static void main(String args[])
+	{
+		Scanner sc=new Scanner(System.in);
+		Scanner s=new Scanner(System.in);
+		float pt,total;
+		System.out.print("Enter the admission number: ");
+		String admsn=sc.nextLine();
+		System.out.print("Name of the student: ");
+		String name=sc.nextLine();
+		System.out.println("Enter the academic and sports details of a student: ");
+		System.out.print("Enter the total CGPA obtained: ");
+		total=s.nextFloat();
+		System.out.print("Enter the overall score point obtained in sports(out of 10): ");
+		pt=s.nextFloat();
+
+		Result o =new Result();
+		o.read1(name,total,admsn);
+		o.read2(pt);
+		o.display();
+	}
 }
